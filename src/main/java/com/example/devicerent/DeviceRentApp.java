fa4a6262dc9634dd91ef6edbe27c5aea70cd942c
DamianJarzebowski <damian.r.hajduk@o2.pl>
2022-02-14 20:49:06 +0100

    WIP

--- src/main/java/com/example/devicerent/DeviceRentApp.java
+++ src/main/java/com/example/devicerent/DeviceRentApp.java
@@ -13,13 +13,6 @@ public class DeviceRentApp {
     public static void main(String[] args) {
         ConfigurableApplicationContext context = SpringApplication.run(DeviceRentApp.class, args);
 
-        /*
-        var categoryRepository = context.getBean(CategoryRepository.class);
-        var customerRepository = context.getBean(CustomerRepository.class);
-        var deviceRepository = context.getBean(DeviceRepository.class);
-         */
-
-
 
     }
 }
